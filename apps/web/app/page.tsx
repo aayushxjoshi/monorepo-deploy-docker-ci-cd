b7520960713670b72fc6914d32748ccca3ff7c41
aayushxjoshi <aayushj9173@gmail.com>
2026-05-02 00:35:36 +0530

    fix ssg nextjs

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,9 @@
 import styles from "./page.module.css";
 import { prisma } from "db/client";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const data = await prisma.user.findMany();
   return <div className={styles.page}>{JSON.stringify(data)}</div>;
 }
-
-export const revalidate = 60;
